d70d6429a7873375ccb831f3c215bed0363a4c2a
Noah Park-Nguyen <noahparknguyen@gmail.com>
2025-01-14 11:59:58 -0500

    updated url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: { extensions: [".js", ".ts", ".tsx", ".jsx"] },
   plugins: [react()],
   base: "/noahpn/",
 });
